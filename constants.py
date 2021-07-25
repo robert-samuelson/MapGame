cdefbaecc2462208eabe1bf7d38de7313725542c
Robert <robert_samuelson@hotmail.com>
2021-07-25 16:22:49 -0700

    Initial Working Version

---
+++ constants.py
@@ -0,0 +1,38 @@
+MAP_FILENAME_PREFIX = "mx"
+MAX_ROUNDS = 6
+MAP_DIRECTORY = "MexicoMaps"
+INITIAL_MAP = MAP_DIRECTORY + "/" + "mmap.jpg"
+
+score_to_state = {11: "Aguascalientes - AGS",
+                  12: "Baja California Norte - BCN",
+                  13: "Baja California Sur - BCS",
+                  14: "Campeche - CAMP",
+                  15: "Chiapas - CHIS",
+                  16: "Chihuahua - CHIH",
+                  17: "Coahuila - COAH",
+                  18: "Colima - COL",
+                  19: "Distrito Federal - DF",
+                  20: "Durango - DGO",
+                  21: "Estado De Mexico - MEX",
+                  22: "Guanajuato - GTO",
+                  23: "Guerrero - GRO",
+                  24: "Hidalgo - HGO",
+                  25: "Jalisco - JAL",
+                  26: "Michoacan - MICH",
+                  27: "Morelos - MOR",
+                  28: "Nayarit - NAY",
+                  29: "Nuevo Leon - NL",
+                  30: "Oaxaca - OAX",
+                  31: "Puebla - PUE",
+                  32: "Queretaro - QRO",
+                  33: "Quintana Roo - QR",
+                  34: "San Luis Potosi - SLP",
+                  35: "Sinaloa - SIN",
+                  36: "Sonora - SON",
+                  37: "Tabasco - TAB",
+                  38: "Tamaulipas - TAMPS",
+                  39: "Tlaxcala - TLAX",
+                  40: "Veracruz - VER",
+                  41: "Yucatan - YUC",
+                  42: "Zacatecas - ZAC"
+                  }
---
+++ main.py
@@ -0,0 +1,84 @@
+#!python3.7
+import tkinter as tk
+from PIL import Image, ImageTk
+from constants import *
+import time
+
+class MapGame:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.image = ImageTk.PhotoImage(Image.open(INITIAL_MAP).resize((800, 400)))
+        self.map_picture = tk.Label(image=self.image)
+        self.question = tk.Label(text="Blah")
+        self.start_game(None)
+        self.yes_button = tk.Button(text="Yes", width=25, height=5, bg="blue", fg="yellow")
+        self.yes_button.bind("<Button-1>", self.yes_func)
+        self.no_button = tk.Button(text="No",  width=25, height=5, bg="red",  fg="yellow")
+        self.no_button.bind("<Button-1>", self.no_func)
+        self.start_over_button = tk.Button(text="Start Over", width=25, height=5, bg="green", fg="yellow")
+        self.start_over_button.bind("<Button-1>", self.start_game)
+        self.map_picture.pack()
+        self.question.pack()
+        self.yes_button.pack()
+        self.no_button.pack()
+        self.start_over_button.pack()
+        self.window.mainloop()
+
+    def start_game(self, event):
+        print("Starting Game...")
+        self.score = 0
+        self.rounds_left = MAX_ROUNDS
+        self.game_over = False
+        self.image = ImageTk.PhotoImage(Image.open(INITIAL_MAP).resize((800, 400)))
+        self.map_picture.configure(image=self.image)
+        response = "Can you find the state where you born in on the map?"
+        self.question.configure(text=response)
+
+    def yes_func(self, event):
+        if self.game_over:
+            self.start_game()
+        if self.rounds_left == MAX_ROUNDS:
+            self.update_round()
+        elif self.rounds_left >= 0:
+            self.score += 2**(self.rounds_left)
+            self.update_round()
+
+    def no_func(self, event):
+        if self.rounds_left == MAX_ROUNDS:
+            self.question.configure(text="FIXME: We are going to have a very difficult time then...")
+            time.sleep(5)
+            exit()
+        if self.game_over:
+            time.sleep(5)
+            exit()
+        self.update_round()
+
+    def game_end(self):
+        if self.score in score_to_state:
+            response = "I think your state is " + score_to_state[self.score]
+        else:
+            response = "You gave me bad input"
+        response += "....   Would you like to play again?"
+        self.question.configure(text=response)
+        self.game_over = True
+
+    def update_round(self):
+        print(f"Score: {self.score} Rounds Left: {self.rounds_left}")
+        if self.rounds_left == 0:
+            self.game_end()
+        else:
+            self.rounds_left -= 1
+            self.update_image()
+
+    def update_image(self):
+        self.new_image = ImageTk.PhotoImage(Image.open(self.round_to_image_file()).resize((800, 400)))
+        self.map_picture.configure(image=self.new_image)
+        self.question.configure(text="Is your state in yellow?")
+
+    def round_to_image_file(self):
+        filepath = MAP_DIRECTORY + "/" + MAP_FILENAME_PREFIX + str(2**(self.rounds_left)) + ".jpg"
+        return filepath
+
+MapGame()
+
+
